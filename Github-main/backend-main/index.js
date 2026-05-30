f402b361a0188d68edba207f94d90c255967cc5c
Ritika Singh <2k23.it2310598@gmail.com>
2026-05-30 13:23:28 +0100

    Update index.js

--- Github-main/backend-main/index.js
+++ Github-main/backend-main/index.js
@@ -72,7 +72,6 @@ yargs(hideBin(process.argv))
   .help().argv;
 
 function startServer() {
-  const app = express();
   const port = process.env.PORT || 5000;
   httpServer.listen(port, () => {
     console.log(`Server is running on PORT ${port}`);
