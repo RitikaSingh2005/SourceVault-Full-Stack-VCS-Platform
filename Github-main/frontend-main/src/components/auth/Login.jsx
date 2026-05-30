4da50113830445fa4e21e9df1765477861c85ecb
Ritika Singh <2k23.it2310598@gmail.com>
2026-05-30 09:01:01 +0100

    Update Login.jsx

--- Github-main/frontend-main/src/components/auth/Login.jsx
+++ Github-main/frontend-main/src/components/auth/Login.jsx
@@ -98,7 +98,7 @@ const Login = () => {
         </div>
         <div className="pass-box">
           <p>
-            New to GitHub? <Link to="/signup">Create an account</Link>
+            New to SourceVault? <Link to="/signup">Create an account</Link>
           </p>
         </div>
       </div>
